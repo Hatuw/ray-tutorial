589637698b3e429802bcb75d02e2fa1ab22082df
hatuw <jiaxi_wu@ieee.org>
2019-01-10 19:41:12 +0800

    add KMeans on ray

---
+++ 2_KMeans/2-KMeans.py
@@ -0,0 +1,11 @@
+import ray
+import numpy as np
+import tensorflow as tf
+
+
+def tf_k_means(vector, no_clusters=3, n_iter=100, verbose=False):
+    pass
+
+
+if __name__ == '__main__':
+    ray.init()
